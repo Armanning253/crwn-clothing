348e1b33cad26cbe4c266d78acec32a93f87e12d
Tony Manning <armanning253@gmail.com>
2020-12-28 18:15:52 -0800

    updating our app with a shop page, collection preview component and collection item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,6 +3,7 @@ import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
 
+
 const Homepage = () => (
     <div className="homepage">
         <Directory />
